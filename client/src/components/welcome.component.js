65a25a51eb5e885df0cbbfb0951fc33fed870798
laialex501 <laialex501@gmail.com>
2020-09-15 11:42:14 -0700

    Normal deployment version

--- client/src/components/welcome.component.js
+++ client/src/components/welcome.component.js
@@ -5,8 +5,6 @@ const debug = require("debug")("welcome.component");
 class Welcome extends React.Component {
   render() {
     debug("Entering Welcome Component");
-    console.log("Node env: ", process.env.NODE_ENV);
-    console.log("Port: ", process.env.PORT);
     return (
       <React.Fragment>
         <div>
