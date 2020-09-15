32a0683e3095f620fcae6ed030624a65524a10b4
laialex501 <laialex501@gmail.com>
2020-09-15 11:03:20 -0700

    Changed .gitignore to allow for build files to be included

---
+++ client/build/precache-manifest.9938142209e93ee81d63a4b9b633d6c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102371ce830811c5687f38dd49a21f1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca37ee945c7357673ecb",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5365bce7844c40b147ee",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ca37ee945c7357673ecb",
+    "url": "/static/js/2.e3adbe25.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e3adbe25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5365bce7844c40b147ee",
+    "url": "/static/js/main.9bbc7def.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
